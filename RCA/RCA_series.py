fe58c9ec7a3246186b6f30ff99657eead0345b24
dannydacosta <51742161+dannydacosta@users.noreply.github.com>
2020-03-20 19:00:05 +0100

    Delete RCA_series.py

--- RCA/RCA_series.py
+++
@@ -1,8 +0,0 @@
-import RCA
-
-inpath = 'C:/Users/Danny/PycharmProjects/RCA/in_folder'
-outpath = 'C:/Users/Danny/PycharmProjects/RCA/out_folder'
-
-RCA1 = RCA.RetinalCompression()
-
-RCA1.series_dist(inpath, outpath)
