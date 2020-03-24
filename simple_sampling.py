1ec4fc708bdf2bbb4b96da76d8ab78e57d86ca26
mario <MSenden>
2020-03-24 19:21:04 +0100

    corrected variable names

--- simple_sampling.py
+++ simple_sampling.py
@@ -10,9 +10,10 @@ class RetinalCompression:
         self.re = 22  # Scale factor of the exponential. Not used in our version.
         self.dg = 33162  # Cell density at r = 0
         self.C = (3.4820e+04 + .1)  # Constant added to the integral to make sure that if x == 0, y == 0.
-        self.inS, self.inR, self.inC, self.inD = int, int, int, int  # Dimensions of the selected image (pixel space)
+        self.size_in, self.inR, self.inC, self.inD = int, int, int, int  # Dimensions of the selected image (pixel space)
         self.bg_color = 127  # Background color for the generated image.
         self.num_pixels_in = [] # Placeholder for number of pixels of input image
+        self.size_in = []
         self.size_out = []
         self.W = None  # Placeholder for spare matrix used for image transformation when using series_dist method
         self.mask = None  # Placeholder for mask when using series_dist method
@@ -33,8 +34,9 @@ class RetinalCompression:
         eccentricity = parameters['field_of_view'] / 2
         radius_in = parameters['resolution_in'] / 2
 
-        self.num_pixels_in = parameters['size_in']**2
-        self.size_out = parameters['size_out']
+        self.size_in = parameters['resolution_in']
+        self.size_out = parameters['resolution_out']
+        self.num_pixels_in = self.size_in**2
         num_pixels_out = self.size_out**2
 
         num_cells = self.fi(eccentricity)
@@ -64,18 +66,17 @@ class RetinalCompression:
             # to the indices 4, and 5. The RGB values are weighted accordingly (0.7 for index 4, and 0.3 for index
             # 5). Additionally, boundary checking is used. Values can never be smaller than 0, or larger than the
             # maximum index of the image.
-            x = np.minimum(np.maximum([floor(new_y[i]), ceil(new_y[i])], 0), self.inS - 1)
-            y = np.minimum(np.maximum([floor(new_x[i]), ceil(new_x[i])], 0), self.inS - 1)
-            c, idx = np.unique([x[0] * self.inS + y, x[1] * self.inS + y], return_index=True)
+            x = np.minimum(np.maximum([floor(new_y[i]), ceil(new_y[i])], 0), self.size_in - 1)
+            y = np.minimum(np.maximum([floor(new_x[i]), ceil(new_x[i])], 0), self.size_in - 1)
+            c, idx = np.unique([x[0] * self.size_in + y, x[1] * self.size_in + y], return_index=True)
             dist = np.reshape(np.array([np.abs(x - new_x[i]), np.abs(y - new_y[i])]), 4)
             self.W[i, c] = dist[idx] / sum(dist[idx])
     
     def distort_image(self, image):
         try:
-            [height, width, depth] = image.shape  # Determine dimensions of the selected image (pixel space)
+            depth =  np.size(img, axis=2) # Determine dimensions of the selected image (pixel space)
         except:
-            [height, width] = image.shape  # Determine dimensions of the selected image (pixel space)
-            height = None
+            depth = None
 
         if depth:
             image = np.reshape(image, (self.num_pixels_in, depth))
